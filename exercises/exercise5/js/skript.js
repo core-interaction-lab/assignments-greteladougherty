4f1d91676a2dcddbbb7dcedee764b4fe9a8b0341
greteladougherty <98603231+greteladougherty@users.noreply.github.com>
2022-03-08 15:49:47 -0500

    update

---
+++ exercises/exercise5/js/skript.js
@@ -0,0 +1,20 @@
+/*
+alert('here we go');
+*/
+const button1=document.getElementById('btn-1');
+
+const button2=document.querySelector('.btn');
+
+const allbuttons=document.querySelectorAll('.btn')
+
+var copy="yeehaw";
+
+
+
+console.log(copy);
+console.log(button1);
+console.log(allbuttons)
+
+button1.addEventListener('click', function (event){
+alert('voila')
+});
