1a8a50cae0e332492ca1b13f041003d3656995d7
greteladougherty <98603231+greteladougherty@users.noreply.github.com>
2022-05-03 19:17:43 -0400

    proj4 initial

---
+++ codestudio/proj4/js/main.js
@@ -0,0 +1,14 @@
+$( function() {
+ $( ".draggable" ).draggable();
+ iframeFix: true;
+ stack: ".draggable"
+ });
+
+// Getter
+var iframeFix = $( ".draggable" ).draggable( "option", "iframeFix" );
+ 
+// Setter
+$( ".draggable" ).draggable( "option", "iframeFix", true );
+
+
+
